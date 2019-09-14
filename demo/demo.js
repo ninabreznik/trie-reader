2f0e149e12753ff874c8d0e892a8ac0c9938d5fd
ninabreznik <ninabreznik@gmail.com>
2019-09-14 01:02:14 +0100

    Make basic trie work

--- demo/demo.js
+++ demo/demo.js
@@ -1,47 +1,71 @@
 const SDK = require('dat-sdk')
-const { Hypercore, Hyperdrive, resolveName, deleteStorage, destroy } = SDK()
+const { Hypercore } = SDK()
 const hypertrie = require('hypertrie')
-const raidb = require('random-access-idb')
+//const raidb = require('random-access-idb')
 
 document.title = 'READER'
+//
+// window.onmessage = ({ source, data}) => {
+//   if (source === window.opener) {
+//     const daturl = data
+//     start(daturl)
+//   }
+// }
+// window.opener.postMessage('ready', '*')
 
-window.onmessage = ({ source, data}) => {
-  if (source === window.opener) {
-    const daturl = data
-    start(daturl)
-  }
-}
-window.opener.postMessage('ready', '*')
+// POLO c5b5d188cf5a3fc570e56beb54ac9ca2df14e8d97e93cde27006a859f274eedb
+const daturl = `dat://bcb472471c2787fa8a5d3cd4f702f9d0d319ed63289d6a6136df6e2ccad19be0`
+start(daturl)
 
 function start (daturl) {
-  console.log('Trying to initialize:')
-  console.log(daturl)
+
+  const feed = new Hypercore(daturl, {valueEncoding: 'binary', sparse: false})
+
+  feed.on('peer-add', (peer) => {
+    console.log('got peer', peer)
+  })
+
+  const trie = hypertrie(null, { feed })
+
+  window.trie = trie
+
+  trie.ready(() => {
+    console.log('Loaded trie', daturl)
+    reallyReady(trie, () => {
+      console.log('READY')
+      viewTrie()
+    })
+  })
+
+  function viewTrie () {
+    window.trie.list('', (err, results) => {
+    	if(err) return log('Error reading from trie', err.message)
+    	for(let {key, value} of results) {
+  		console.log('key:', key, 'value:', Buffer.from(value).toString('utf8'))
+    	}
+    })
+  }
 
   // const db = hypertrie(null, daturl, {
   //   valueEncoding: 'json',
-  //   feed: new Hypercore(filename => (console.log({filename}), raidb())),
+  //   feed: new Hypercore(url, {valueEncoding: 'binary', sparse: false}),
+  //
   //   // extensions: ['discovery']
   // })
-
-  // reallyReady(db, () => {
-  //   console.log('READY')
-  //   db.list((err, nodes) => nodes.forEach(node => {
-  //     console.log(`${node.key}: ${node.value.toString('utf-8')}`)
-  //   }))
-  // })
+  //
 
   //
   // // This make sure you sync up with peers before trying to do anything with the archive
-  // function reallyReady (archive, cb) {
-  //   debugger
-  //   if (archive.metadata.peers.length) {
-  //     archive.metadata.update({ ifAvailable: true }, cb)
-  //   } else {
-  //     archive.metadata.once('peer-add', () => {
-  //       archive.metadata.update({ ifAvailable: true }, cb)
-  //     })
-  //   }
-  // }
+  function reallyReady (trie, cb) {
+    if (trie.feed.peers.length) {
+      trie.feed.update({ ifAvailable: true }, cb)
+      viewTrie()
+    } else {
+      trie.feed.once('peer-add', () => {
+        trie.feed.update({ ifAvailable: true }, cb)
+      })
+    }
+  }
 
   // db.createReadStream()
   //   .on('data', data => console.log(data.key))
